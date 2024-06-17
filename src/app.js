5eb4be819842cbe3b0c45bab15b0165f7445eaed
Federico Requena <federico.requena@gmail.com>
2024-06-17 22:18:58 +0000

    Pending changes exported from your codespace

--- src/app.js
+++ src/app.js
@@ -4,8 +4,38 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+//.   0            1             2            3         4
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+function getRandomNumber(a, b) {
+  return Math.floor(Math.random() * (b - a) + a);
+}
+
+function getRandom(anyArray) {
+  let max = anyArray.length - 1;
+  let min = 0;
+  let random = getRandomNumber(max, min);
+  return anyArray[random];
+}
 
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.body.innerHTML =
+    getRandom(who) +
+    " " +
+    getRandom(action) +
+    " " +
+    getRandom(what) +
+    " " +
+    getRandom(when);
 };
